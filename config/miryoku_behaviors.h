ecc8e7c12a843e2ff3e5d5d96a96d0b0e20a88bb
jayantp <jayantp@sahaj.ai>
2023-01-18 22:40:39 -0600

    reverting to normal

--- config/miryoku_behaviors.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2022 Manna Harbour
-// https://github.com/manna-harbour/miryoku_zmk/blob/master/miryoku/miryoku_behaviors.h
-
-#pragma once
-
-#define U_MT(MOD, TAP) &u_mt MOD TAP
-#define U_LT(LAYER, TAP) &u_lt LAYER TAP
